637f78228bff494b4d3d8951921773587cc66aa4
lst <937985561@qq.com>
2024-05-07 19:19:51 +0800

    bell sb

---
+++ demo6/src/test/java/com/example/demo6/Demo6ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo6;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ demo6/src/test/java/com/example/demo6/domain/Comment.java
@@ -0,0 +1,9 @@
+package com.example.demo6.domain;
+
+/**
+ * @author 梁松涛
+ * @version 1.0
+ **/
+public class Comment {
+    
+}
