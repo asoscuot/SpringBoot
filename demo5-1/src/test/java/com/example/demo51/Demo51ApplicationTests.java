e58a9eb6f9e4894a165d1fb5f065f985078177c0
lst <937985561@qq.com>
2024-05-07 14:50:40 +0800

    sb

---
+++ demo5-1/src/test/java/com/example/demo51/Demo51ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo51;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo51ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
